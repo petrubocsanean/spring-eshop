da0a8521e4610a49623674067bb2700529427d9d
Petru Bocsanean <petru.bocsanean@gmail.com>
2020-06-24 19:32:49 +0200

    first commit

---
+++ src/main/java/com/petrubocsanean/springeshop/HomeController.java
@@ -0,0 +1,12 @@
+package com.petrubocsanean.springeshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    String home() {
+        return "home";
+    }
+}
---
+++ src/main/java/com/petrubocsanean/springeshop/SpringEshopApplication.java
@@ -0,0 +1,13 @@
+package com.petrubocsanean.springeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEshopApplication.class, args);
+	}
+
+}
